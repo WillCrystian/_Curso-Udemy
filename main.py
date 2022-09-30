a924e8b695a2a1392d196f327e66c05d2681e02c
willcrystian <willcrystian@gmail.com>
2022-09-30 10:42:30 -0300

    class Pessoa

---
+++ main.py
@@ -0,0 +1,11 @@
+from modulo_4.pessoa import Pessoa
+
+p1 = Pessoa('William', 30)
+
+p1.falar('POO')
+p1.comer('abacate')
+p1.parar_falar()
+p1.comer('abacate')
+p1.falar('agv')
+p1.parar_comer()
+p1.falar('agv')
