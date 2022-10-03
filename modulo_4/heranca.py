ce7c3256e6085f3015987b4dd797b93b0e643111
willcrystian <willcrystian@gmail.com>
2022-10-03 08:01:18 -0300

    sobreescrevendo metodos de herança

--- modulo_4/heranca.py
+++ modulo_4/heranca.py
@@ -7,9 +7,17 @@ class Pessoa:
 class Cliente(Pessoa):
     def comprar(self):
         print('Cliente comprando')
+        
+class Aluno(Pessoa):
+    def __init__(self, nome, idade, sobrenome):
+        super().__init__(nome, idade) # como sobrepor um metodo
+        #Pessoa.__init__(self, nome, idade) # passa o nome da classe e  self
+        self.sobrenome = sobrenome
     
     
 c1 = Cliente('Robson', 31)
 print(c1.nome, c1.idade)
 c1.comprar()
-#a1 = Aluno('Manuela', 16)
+
+a1 = Aluno('Manuela', 16, 'Ferreira')
+print(a1.nome, a1.sobrenome, a1.idade)
